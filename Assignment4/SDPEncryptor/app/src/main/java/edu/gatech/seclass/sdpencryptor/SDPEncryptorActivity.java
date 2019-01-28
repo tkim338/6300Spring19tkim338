366529bfc2a61a0f52c807ab1d830061ace6bc3f
Thomas Kim <tkim338@gatech.edu>
2019-01-27 20:32:01 -0500

    added error messages

--- Assignment4/SDPEncryptor/app/src/main/java/edu/gatech/seclass/sdpencryptor/SDPEncryptorActivity.java
+++ Assignment4/SDPEncryptor/app/src/main/java/edu/gatech/seclass/sdpencryptor/SDPEncryptorActivity.java
@@ -2,48 +2,116 @@ package edu.gatech.seclass.sdpencryptor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.TextView;
+import java.util.ArrayList;
 
 public class SDPEncryptorActivity extends AppCompatActivity {
 
-    static String Ciphertext = "";
+    static String Ciphertext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView messageTextView = findViewById(R.id.messageText);
+        final TextView rotateTextView = findViewById(R.id.rotateNumber);
+        final TextView shiftTextView = findViewById(R.id.shiftNumber);
+        messageTextView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                for (int e = 0; e < s.length(); e++) {
+                    if (!Character.isLetter(s.charAt(e))) {
+                        messageTextView.setError("Alphabetic Message Required");
+                        return;
+                    }
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+        shiftTextView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length()==0) {
+                    if (rotateTextView.getText().length() == 0) {
+                        shiftTextView.setError("No Encryption Applied");
+                        return;
+                    }
+                }
+                else {
+                    int shiftInt = Integer.parseInt(s.toString());
+                    if (shiftInt < 0 || shiftInt > 25) {
+                        shiftTextView.setError("Must Be Between 0 And 25");
+                        return;
+                    }
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+        rotateTextView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length()==0 && shiftTextView.getText().length()==0) {
+                    rotateTextView.setError("No Encryption Applied");
+                    return;
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
     }
 
     // encrypt message
     public void encryptMessage(View view) {
+        ArrayList<Integer> charIndex = new ArrayList<>();
+        Ciphertext = "";
         TextView messageTextView = findViewById(R.id.messageText);
         String inputMessage = messageTextView.getText().toString();
         TextView rotateTextView = findViewById(R.id.rotateNumber);
-        int rotateInt = Integer.parseInt(rotateTextView.getText().toString());
         TextView shiftTextView = findViewById(R.id.shiftNumber);
+
+        int rotateInt = Integer.parseInt(rotateTextView.getText().toString());
         int shiftInt = Integer.parseInt(shiftTextView.getText().toString());
 
         int messageLength = inputMessage.length();
         for (int i = 0; i < messageLength; i++) {
+            charIndex.add((i + messageLength - rotateInt) % messageLength);
+
             int charAscii = (int) inputMessage.charAt(i);
             String shiftedChar;
             int ascii_A = 65;
             int ascii_a = 97;
             int shiftFactor;
             if (charAscii >= ascii_a) { //lowercase
-                shiftFactor = ascii_a + 1;
+                shiftFactor = ascii_a-1;
             }
             else {
-                shiftFactor = ascii_A + 1;
+                shiftFactor = ascii_A-1;
             }
             shiftedChar = Character.toString((char)((( ((charAscii-shiftFactor) + shiftInt) -1) % 26)+1 + shiftFactor));
             //Ciphertext = Ciphertext + String.valueOf(charAscii) + " " + String.valueOf((charAscii + shiftInt - shiftFactor)%26 + shiftFactor);
             Ciphertext = Ciphertext + String.valueOf(shiftedChar);
         }
 
+        String Ciphertext2 = "";
+        for (int j = 0; j < messageLength; j++) {
+            Ciphertext2 = Ciphertext2 + Ciphertext.substring((Integer)charIndex.get(j),(Integer)charIndex.get(j)+1);
+        }
+
         //Ciphertext = inputMessage;
         TextView messageOutputView = findViewById(R.id.resultText);
-        messageOutputView.setText(Ciphertext);
+        messageOutputView.setText(Ciphertext2);
     }
 }
